edf3165150292764ef81d600837c34007038696b
jdistefano1 <jdistefano@mtb.com>
2016-05-31 11:51:33 -0400

    ch 14

---
+++ Ch4/ch14.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    
+    // start up code goes here
+   // alert("this works!");
+    //jQuery(document).ready(function(){$.document.fadeout(500).fadein(500);
+
+    $("#title").text("Yay, I can now get at the H1 immediately!");
+   // $("#title").text.fadeout(500);
+
+    $("#first").html("<h2>Great quotes</h2>");
+    
+
+    // append and prepend work INSIDE the given selection
+    $("#first").prepend("<h2>Great quotes</h2>");
+    $("#first").append("<h3>... for your to ponder ... </h3>");
+
+    // before, after, insertBefore, insertAfter work OUTSIDE
+    // the given selection.
+   // $("#first").fadeout(100);
+    
+
+    $("#myAnchor").attr("href", "http://channel9.msdn.com");
+
+    $("#title").addClass("standout");
+
+});
+
+
+
