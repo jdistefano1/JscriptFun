0324dafda5b976975198dc4fe0ff35076438e1f0
jdistefano1 <jdistefano@mtb.com>
2016-05-31 11:01:28 -0400

    thru 13

--- Ch4/ch12.js
+++ Ch4/ch12.js
@@ -43,3 +43,43 @@ var planet = {
         { locationid: 17, name: "meaty" }
     ]
 };
+
+//planet.faction.notification();
+//document.write(planet.cities[1].name);
+
+/*
+document.write("<br />" + planet.name);
+planet.name = "turk";
+document.write("<br />" + planet.name);
+*/
+
+/*
+var z = planet;
+
+document.write(z.name);
+
+if (typeof planet.defence === "undefined") {
+    planet.defense = "ion canon";
+   
+}
+document.write(planet.defense)
+*/
+
+/*
+for (member in planet) {
+    document.write("<br />" + member + " " + planet[member]);
+}
+*/
+
+function car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+var myCar = new car("Eagle", "Talon SI", 1993);
+var myOtherCar = new car("Chevy", "S10", 2016);
+
+
+alert(myCar.model);
+alert(myOtherCar.year);
